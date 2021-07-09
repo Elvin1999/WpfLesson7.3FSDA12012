0682ef9773fc3743aa032d1859bc49fd6b0ba402
Elvin <elvincamalzade00@gmail.com>
2021-07-09 16:51:25 +0400

    Add project files.

---
+++ WpfLesson7.3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfLesson7._3.Commands;
+
+namespace WpfLesson7._3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public RelayCommand MessageCommand { get; set; }
+        public RelayCommand SendCommand { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+            MessageCommand = new RelayCommand(Display);
+            SendCommand = new RelayCommand(Send);
+
+        }
+
+        public void Display(object text = null)
+        {
+            MessageBox.Show("Hello Relay Command");
+        }
+
+        public void Send(object text = null)
+        {
+            MessageBox.Show("Message was sent");
+        }
+
+    }
+}
---
+++ WpfLesson7.3/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLesson7._3.Models
+{
+   public class Car:Entity
+    {
+        public string Vendor { get; set; }
+    }
+}
